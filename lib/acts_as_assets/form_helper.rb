3d85c54d4c6288dea7dac7014ae9ffb840de2e1f
Gian Carlo Pace <giancarlo.pace@gmail.com>
2012-05-24 18:25:35 +0200

    Added a read_only option to partials

--- lib/acts_as_assets/form_helper.rb
+++ lib/acts_as_assets/form_helper.rb
@@ -1,11 +1,11 @@
 module ActionView
   module Helpers
     class FormBuilder
-      def asset_upload(method, type)
-        @template.asset_upload_tag(@object, method, type)
+      def asset_upload(method, type, *args)
+        @template.asset_upload_tag(@object, method, type, *args)
       end
-      def asset_multiple_upload(method, type)
-        @template.asset_multiple_upload_tag(@object, method, type)
+      def asset_multiple_upload(method, type, *args)
+        @template.asset_multiple_upload_tag(@object, method, type, *args)
       end
       def asset_label(method, type)
         @template.asset_label_tag(@object, method, type)
@@ -13,12 +13,14 @@ module ActionView
     end
 
     module FormTagHelper
-      def asset_multiple_upload_tag(model, methods, type)
-        asset_upload_helper(model, methods, type, "acts_as_assets/assets/asset_multiple_upload")
+      def asset_multiple_upload_tag(model, methods, type, *args)
+        options = args.extract_options!
+        asset_upload_helper(model, methods, type, "acts_as_assets/assets/asset_multiple_upload", options)
       end
 
-      def asset_upload_tag(model, methods, type)
-        asset_upload_helper(model, methods, type, "acts_as_assets/assets/asset_upload")
+      def asset_upload_tag(model, methods, type, *args)
+        options = args.extract_options!
+        asset_upload_helper(model, methods, type, "acts_as_assets/assets/asset_upload", options)
       end
 
       private
@@ -28,9 +30,9 @@ module ActionView
       # for instance
       # asset_upload_tag(@pratica, [:back_office, :allegati], ...)
       # will call @pratica.back_office.allegati
-      def asset_upload_helper(model, methods, type, partial)
+      def asset_upload_helper(model, methods, type, partial, options)
         render :partial => partial,
-               :locals => {:@type => type, :model => model, :assets => model_assets(methods, model)}
+               :locals => {:@type => type, :model => model, :assets => model_assets(methods, model), read_only: options[:read_only]}
       end
 
       def model_assets(methods, model)
